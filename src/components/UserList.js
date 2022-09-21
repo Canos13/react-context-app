1d7f961033dcdbe2c152e2ef1ee9d3f9746d84ce
canos13 <canomsergio13@gamil.com>
2022-09-21 17:14:36 -0500

    Mostramos la lista de usuarios

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,18 +1,39 @@
 import React, { useEffect } from 'react'
 import { useContext } from 'react'
 import UserContext from '../context/User/UserContext'
+import app from '../styles/app.module.css'
 
 const UserList = () => {
 
-  const { getUsers } = useContext(UserContext)
+    const { user, getUsers, getProfile  } = useContext(UserContext)
 
-  useEffect(() => {
-    getUsers()
-  }, [])
+    useEffect(() => {
+      getUsers()
+    }, [])
 
-  return (
-    <div>UserList</div>
-  )
+    return (
+        <div className={app.container}>
+            {
+                user.map((user, i)=>{
+                    return (
+                        <div 
+                          key={i}
+                          onClick={ () => getProfile(user.id) }
+                          className={app.d_flex}
+                          >
+                            <img 
+                                src={user.avatar} 
+                                alt={user.first_name}
+                                className={app.img_rounded} />
+                            <h2 className={app.h2}>
+                              {`${user.first_name} ${user.last_name}`} 
+                            </h2>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default UserList
