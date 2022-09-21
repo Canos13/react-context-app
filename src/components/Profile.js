39cc4b89390d7e5aa1f143f02f5cc848d0a968cc
canos13 <canomsergio13@gamil.com>
2022-09-21 17:14:20 -0500

    pintamos el profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import UserContext from '../context/User/UserContext'
+import profile from '../styles/profile.module.css'
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+
+    const { selectedUser } = useContext(UserContext);
+
+    return (
+        <>
+            {
+                selectedUser ? (
+                      <section className={profile.text_center}>
+                          <img 
+                              src={selectedUser.avatar} 
+                              alt={selectedUser.first_name}
+                              className={profile.img} />
+                          <div>
+                              {`${selectedUser.first_name} ${selectedUser.last_name}`}
+                          </div>
+                          <div>{selectedUser.email}</div>
+                      </section>
+                ) : (
+                    <section className={profile.text_center}>Selecciona un usuario</section>
+                )
+            }
+        </>
+    )
 }
 
 export default Profile
